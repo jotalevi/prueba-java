85ee19fbd104366dbb33952ec9d3912c63201bd2
jotalevi <etalevi@moveapps.cl>
2023-11-21 17:23:04 -0300

    initial commit

---
+++ src/prueba3java/Prueba3Java.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba3java;
+
+/**
+ *
+ * @author neto
+ */
+public class Prueba3Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
