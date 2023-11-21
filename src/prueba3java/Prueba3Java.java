7a8a3a2bb7c7b0744307603d264a50f6147b66c4
jotalevi <etalevi@moveapps.cl>
2023-11-21 17:42:07 -0300

    Chupalo DJ

--- src/prueba3java/Prueba3Java.java
+++ src/prueba3java/Prueba3Java.java
@@ -14,7 +14,7 @@ public class Prueba3Java {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("chupalo DJ");
     }
-    
+
 }
