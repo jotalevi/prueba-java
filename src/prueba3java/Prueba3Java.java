4812bc08a63703999258e823fd65e8aa858f6a0a
panamaxi <maximilianoduarte824@gmail.com>
2023-11-21 18:08:42 -0300

    agrege variable andres

--- src/prueba3java/Prueba3Java.java
+++ src/prueba3java/Prueba3Java.java
@@ -10,6 +10,8 @@ package prueba3java;
  */
 public class Prueba3Java {
     static String contrasenia = "MAxiCuLiAOO";
+    
+    static String andres = "pasa la plata ";
 
     /**
      * @param args the command line arguments
